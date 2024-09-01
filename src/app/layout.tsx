36ff85c3d361bfd6327f31f443a23f7d6762f978
openarahmed <openarahmed@gmail.com>
2024-09-01 14:42:43 +0600

    initial commit

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import Navbar from "./components/shared/navbar/navbar";
+import Footer from "./components/shared/footer/footer";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -16,7 +18,10 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        {" "}
+        <Navbar></Navbar> {children} <Footer></Footer>
+      </body>
     </html>
   );
 }
