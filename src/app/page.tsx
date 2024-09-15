3d45ca97fb2cc14b2b6ef5bc7aed3df60405c029
codermat03 <codermatteam@gmail.com>
2024-09-15 18:47:48 +0600

    deleted page.tsx

--- src/app/page.tsx
+++
@@ -1,9 +0,0 @@
-export default function Home() {
-  return (
-    <main className="bg-[#280D42] text-white">
-      <h1 className="text-3xl font-bold ">Welcome to CoderMat world!!</h1>
-    </main>
-  );
-
-
-}
