8b35c2782c1fbbf869a1d1f5743ebcd1558aa5cf
codermat03 <codermatteam@gmail.com>
2024-09-15 18:43:51 +0600

    page.tsx updated

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,7 @@
-import Services from "./components/shared/services/services";
-import ProjectDiscuss from "./components/shared/projectdiscuss/ProjectDiscuss";
-import TechnologyStack from "./components/shared/technologyStack/TechnologyStack";
-
 export default function Home() {
   return (
     <main className="bg-[#280D42] text-white">
       <h1 className="text-3xl font-bold ">Welcome to CoderMat world!!</h1>
-
     </main>
   );
 }
