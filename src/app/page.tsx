49cdb8b4e7452f53ba399146e42606461312113b
codermat03 <codermatteam@gmail.com>
2024-09-15 18:41:02 +0600

    deleted section's imports

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,9 +6,7 @@ export default function Home() {
   return (
     <main className="bg-[#280D42] text-white">
       <h1 className="text-3xl font-bold ">Welcome to CoderMat world!!</h1>
-      <Services />
-      <ProjectDiscuss />
-      <TechnologyStack />
+
     </main>
   );
 }
