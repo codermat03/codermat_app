3119e966a934f7e10635e52b7cf6362953d976b7
codermat03 <codermatteam@gmail.com>
2024-11-18 13:04:12 +0600

    error solved on blog details page

--- src/app/interface.ts
+++ src/app/interface.ts
@@ -8,22 +8,22 @@ export interface Stack {
     id: number;
     name: string;
 }
-export interface Article {
-    id: number;
-    title: string;
-    image: string;
-    description: string;
-    category: string;
-    subTitle: string;
-    cardImage: string;
-    content: {
-        introduction: string;
-        section1: { title: string; body: string };
-        section2: { title: string; body: string };
-        // Add other sections if necessary
-        conclusion: string;
-    };
-}
+// export interface Article {
+//     id: number;
+//     title: string;
+//     image: string;
+//     description: string;
+//     category: string;
+//     subTitle: string;
+//     cardImage: string;
+//     content: {
+//         introduction: string;
+//         section1: { title: string; body: string };
+//         section2: { title: string; body: string };
+//         // Add other sections if necessary
+//         conclusion: string;
+//     };
+// }
 export interface StackCardProps {
     stack: {
         stack: {
@@ -64,4 +64,27 @@ export interface Project {
     liveLink?: string;
     clientCode?: string;
     serverCode?: string;
+}
+
+export interface Content {
+    introduction: string;
+    section1?: { title: string; body: string };
+    section2?: { title: string; body: string };
+    section3?: { title: string; body: string };
+    section4?: { title: string; body: string };
+    section5?: { title: string; body: string };
+    section6?: { title: string; body: string };
+    section7?: { title: string; body: string };
+    conclusion?: string;
+}
+
+export interface Article {
+    id: number | string;
+    title: string;
+    image?: string;
+    description: string;
+    category: string;
+    subTitle: string;
+    cardImage: string;
+    content: Content;
 }
